0ad370c067a9f3222987d82ee4b2be24835a56ad
潘磊 <15060059@suning.com>
2021-12-09 17:01:26 +0800

    代码优化 / 新增强缓存时间带单位配置

--- src/PStatic.js
+++ src/PStatic.js
@@ -1,15 +1,43 @@
-var fs = require('fs')
-var url = require('url')
-var path = require('path')
-var crypto = require('crypto')
-var PStatic = function(dir,opts){
-    var defaultOpt = {
+let fs = require('fs')
+let url = require('url')
+let crypto = require('crypto')
+let pStatic = function(dir,opts){
+    let defaultOpt = {
+        htmlNoCache:false,//支持配置html、htm后缀文件配置无缓存 true 表示禁用缓存
         maxAge:0,
-        LastModify:true,
-        Etag:false,
+        lastModify:true,
+        etag:false,
         defaultPage:'index.html'
     }
     opts = Object.assign(defaultOpt,opts)
+    //支持强缓存时间带单位
+    if(opts.maxAge != 0){ 
+        
+        ((time)=>{
+            let unit = time.charAt(time.length - 1)
+            let val = +time.substr(0,time.length-1)
+            switch (unit){
+                case 's':
+                   opts.maxAge = val;
+                   break;
+                case 'm':
+                   opts.maxAge = val * 60;
+                   break;
+                case 'h':
+                    opts.maxAge = val * 3600;
+                   break;
+                case 'd':
+                   opts.maxAge = val * 3600 * 24;
+                   break;
+                case 'M':
+                   opts.maxAge = val * 3600 * 24 * 30;
+                   break;
+                case 'y':
+                   opts.maxAge = val * 3600 * 24 * 30 * 365;
+                   break;
+            }
+        })(opts.maxAge)
+    }
     /*
     //express sendFile方法基本满足要求,4.*版本目前不支持etag配置
     let a = (req,res,next)=>{
@@ -18,10 +46,10 @@ var PStatic = function(dir,opts){
         let target = url.parse(req.url).pathname
         res.sendFile(__dirname+dir+target,option)
     }*/
-    let myFun = (req,res,next)=>{
+    function myFun(req,res,next){
         let target = url.parse(req.url).pathname;//取当前请求路径
-        let typeArr = ['htm','html','css','js'];//分类别设置res头
-        let typeImg = ['png','jpg','jpeg'];
+        const typeArr = ['htm','html','css','js'];//分类别设置res头
+        const typeImg = ['png','jpg','jpeg'];
         if(target.indexOf('.')== -1){//无后缀路径
             target = target+opts.defaultPage
         }
@@ -33,47 +61,80 @@ var PStatic = function(dir,opts){
             ContentType = 'image/'+fileType
         }
         //根据配置项设置响应对象头
-        var header = {
+        let header = {
             'Content-Type':ContentType,
             'Cache-Control':'max-age='+opts.maxAge
         }
-        if(opts.LastModify == true){
-           var stat = fs.statSync('.'+dir+target);
-           header['Last-Modified'] = stat.mtime.toGMTString()
-        }
+        var lastModifyTime = ''
         var etag = ''
-        if(opts.Etag){
-            fs.readFile('.'+dir+target,function(err,data){
-                etag = crypto.createHash('md5').update(data).digest('hex')
-                header['Etag'] = etag
-                sendFile()
-            })  
-        }else{
+        if((fileType === 'html' || fileType === 'htm') && opts.htmlNoCache){
+            header['Cache-Control'] = 'no-cache'
             sendFile()
+        }else{
+            let p1 = new Promise(function(resolve,reject){
+                if(opts.lastModify){
+                    fs.stat('.'+dir+target,function(err,stats){
+                        if(err){
+                            console.log('文件状态读取失败')
+                        }
+                        else{
+                            lastModifyTime = stats.mtime.toGMTString()
+                            header['Last-Modified'] = lastModifyTime
+                        }
+                        resolve()
+                    }); 
+                }else{
+                    resolve()
+                }
+            })
+            let p2 = new Promise(function(resolve,reject){
+                if(opts.etag){
+                    fs.readFile('.'+dir+target,function(err,data){
+                        if(err){
+                            console.log("文件读取失败")
+                        }
+                        else{
+                            etag = crypto.createHash('md5').update(data).digest('hex')
+                            header['Etag'] = etag
+                        }
+                        resolve()
+                    })  
+                }else{
+                    resolve()
+                }
+            })
+            Promise.all([p1,p2]).then(()=>{
+                sendFile()
+            })
+             
         }
         //根据配置项设置响应对象头
 
-        var sendFile = ()=>{
+        function sendFile(){
              //设置响应码
-            var status = '200'
-            if(opts.LastModify && stat.mtime.toGMTString() === req.headers['if-modified-since'] || opts.Etag && etag === req.headers['if-none-match']){
+            let status = '200'
+            if(!(opts.htmlNoCache && (fileType == 'html'|| fileType == '.htm')) && (opts.lastModify && lastModifyTime === req.headers['if-modified-since'] || opts.etag && etag === req.headers['if-none-match'])){
                 status = '304'
-            }
-            fs.readFile('.'+dir+target,function(err,data){
-                if (err) {
-                    console.log("失败");
-                    console.log(err);
-                } 
                 res.writeHead(status,header)
-                res.end(data)
-            })
-        }
+                res.end()
+            }else{
+                fs.readFile('.'+dir+target,function(err,data){
+                    if (err) {
+                        res.writeHead('404')
+                        res.end('资源未找到')
+                    } else {
+                        res.writeHead(status,header)
+                        res.end(data)
+                    }
+                    
+                })
+            }
             
-       
+        } 
         
     }
     return myFun
    
 }
-module.exports = PStatic
+module.exports = pStatic
     
--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 var express = require('express')
-var path = require('path')
-var Router = express.Router()
-var PStatic =require('./PStatic') //自定义中间件
-console.log(PStatic)
+var pStatic =require('./PStatic') //自定义中间件
 
 const app = express();
-app.use(PStatic('/static',{maxAge:0,LastModify:true,Etag:true}))
+app.use(pStatic('/static',{maxAge:'1d',lastModify:true,etag:false,htmlNoCache:true}))
 app.listen(8888,function(){
     console.log("serving on 8888")
 })
